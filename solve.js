0b32b5184b04263d6f2f426c3013df67180c48fa
suhaboncukcu <suhaboncukcu@gmail.com>
2015-04-17 23:50:45 +0300

    solver

---
+++ solve.js
@@ -0,0 +1,26 @@
+function h() {
+    $arr = $('#box span');
+    $k = $('#box span:nth-child(1)').css('background-color');
+    $count = 0;
+    $tmp =0;
+    for(var $i =1; $i<= $arr.length; $i++) {
+        $fc = $('#box span:nth-child('+$i+')').css('background-color');
+        if($fc == $k) {
+            $count++;
+        } else {
+            $m = $fc;
+            $tmp = $i;
+        }
+    }
+
+    if($count <= 1) {
+        $tmp = 1;
+    }
+
+
+    $('#box span:nth-child('+$tmp+')').css('background-color','white');
+    $('#box span:nth-child('+$tmp+')').attr('id','thing');
+    $('#thing').click();
+}
+
+setInterval(function(){ h(); }, 1);
